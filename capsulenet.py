99aa62bd3a31bc3f37305f332524849ea465715e
Victor <victor.vicente@southworks.com>
2021-01-24 21:56:29 +0100

    change loss to binary crossentropy

--- capsulenet.py
+++ capsulenet.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
     output = Lambda(lambda x: K.sqrt(K.sum(K.square(x), 2)), output_shape=(classes,))(capsule)
 
     model = Model(inputs=input_image, outputs=output)
-    model.compile(loss='mse',#margin_loss,
+    model.compile(loss='binary_crossentropy',#margin_loss,
                   optimizer='adam',
                   metrics=['accuracy'])
 
